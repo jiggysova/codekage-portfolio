ec78b8f26e93d8b01c3b2790b35f841838492674
Jeremiah Pusey Lawrence <jiggyempires@icloud.com>
2025-12-18 23:15:01 +0000

    Project names render

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,8 +11,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const projects = [{ name: "AI Assistant", tech: "Azure", link: "gmail"}, { name: "CyberSecurity Dashboard", tech: "Cisco", link: "Comptia A+"}      
     ];
+        const portfolio = document.getElementById("portfolio");
         projects.forEach((project) => {
             console.log(project);
+
+            const projectDiv = document.createElement("div");
+            projectDiv.textContent = project.name;
+            portfolio.appendChild(projectDiv);
         });
         
      
