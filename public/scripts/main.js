bfa0ebb1f95535e52bfdc074ea020dbec5927a24
Jeremiah Pusey Lawrence <jiggyempires@icloud.com>
2025-12-22 03:11:42 +0000

    Finish dynamic project cards with styling

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const portfolio = document.getElementById("portfolio");
+    if (!portfolio) return;
     const button = document.getElementById("view-projects");
-    const projects = document.getElementById("projects");
+    const projectsSection = document.getElementById("projects");
     
     const projects = [{ name: "AI Assistant", tech: "Azure", link: "gmail"}, 
         { name: "CyberSecurity Dashboard", tech: "Cisco", link: "Comptia A+"}      
@@ -8,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (button) {
         button.addEventListener("click", () => {
-        projects.scrollIntoView({ behavior: "smooth" });
+        projectsSection.scrollIntoView({ behavior: "smooth" });
     });
 }
 
@@ -20,19 +22,20 @@ document.addEventListener("DOMContentLoaded", () => {
             const title = document.createElement("h3");
             title.textContent = project.name;
             projectDiv.appendChild(title);
-            projectDiv.classList.add("project-title");
+            title.classList.add("project-title");
 
             
             const tech = document.createElement("p");
-            tech.textContent = "Tech" + project.tech;
+            tech.textContent = "Tech: " + project.tech;
             projectDiv.appendChild(tech);
-            projectDiv.classList.add("project-tech");
+            tech.classList.add("project-tech");
 
             
             const link = document.createElement ("a");
-            link.textContent = "View Project" + project.link;
-            project.appendChild(link);
-            projectDiv.classList.add("project.link");
+            link.textContent = "View Project";
+            link.href = project.link;
+            projectDiv.appendChild(link);
+            link.classList.add("project-link");
 
             portfolio.appendChild(projectDiv);
         });
